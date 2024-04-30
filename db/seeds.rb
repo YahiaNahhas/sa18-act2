a262c88ea9e0a64ccc2949a32dfe8d30588a3337
Yahia Nahhas <yahianahhas15@gmail.com>
2024-04-29 22:53:09 -0500

    Finished and Ran, Works Good

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+Product.create(name: "Caviar", price: 10.99, description: "Fresh Caviar Served with a Marble Spoon From the Fish of The Atlantic Ocean")
+Product.create(name: "Lamb Chops", price: 19.99, description: "Grass-fed Lamb from the Moutains of Australia")
+Product.create(name: "Shrimp", price: 25.49, description: "Wild-Sought Fresh Shrimp from the Depths of the City of Atlantis")
